9ceed5297b8756e5a5af09ad596ef582ed1378a3
Jerome Farin <j.farin38@gmail.com>
2019-05-27 16:06:45 +0200

    phpunit

---
+++ tests/BookControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Application\Controller\BookController;
+
+class BookControllerTest extends Testcase
+{
+    public function testBookHasCreated()
+    {
+        $this->assert();
+    }
+}
